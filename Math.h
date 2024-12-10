217aaab4489173c95d1d90c3eb17d817c2f31363
harttman <dakiv.offical.vitaliy@gmail.com>
2024-12-10 10:46:33 +0200

    Create Math.h

---
+++ Math.h
@@ -0,0 +1,9 @@
+#ifndef MATH_H
+#define MATH_H
+class Math {
+  static void Add(int x, int y, int* result);
+  static void Sin(int x, int y, int* result);
+  static void Cos(int x, int y, int* result);
+}
+
+#endif // MATH_H
